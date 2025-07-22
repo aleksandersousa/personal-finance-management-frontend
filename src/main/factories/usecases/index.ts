3d2957316be9481642ec4f8abb418000fa9041e7
Aleksander Sousa <aleksander-ssousa@outlook.com>
2025-07-22 18:01:33 -0300

    feat: create factory function for RemoteLoadMonthlySummary to streamline monthly summary data loading

--- src/main/factories/usecases/index.ts
+++ src/main/factories/usecases/index.ts
@@ -2,5 +2,6 @@ export * from './add-entry-factory';
 export * from './authentication-factory';
 export * from './delete-entry-factory';
 export * from './load-entries-by-month-factory';
+export * from './load-monthly-summary-factory';
 export * from './refresh-token-factory';
 export * from './update-entry-factory';
