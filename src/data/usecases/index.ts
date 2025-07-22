10cdee9e70b43a689d4d01335bf3e8f9ca850fed
Aleksander Sousa <aleksander-ssousa@outlook.com>
2025-07-22 18:00:41 -0300

    feat: implement RemoteLoadMonthlySummary class for fetching monthly summary data via HTTP

--- src/data/usecases/index.ts
+++ src/data/usecases/index.ts
@@ -2,5 +2,6 @@ export * from './remote-add-entry';
 export * from './remote-authentication';
 export * from './remote-delete-entry';
 export * from './remote-load-entries-by-month';
+export * from './remote-load-monthly-summary';
 export * from './remote-refresh-token';
 export * from './remote-update-entry';
