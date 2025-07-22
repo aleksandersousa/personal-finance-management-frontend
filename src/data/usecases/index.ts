597d5249ca40f36f87630e7d2e64fb851bcf63aa
Aleksander Sousa <aleksander-ssousa@outlook.com>
2025-07-22 17:21:50 -0300

    feat: export remote-refresh-token and add remote-update-entry use case

--- src/data/usecases/index.ts
+++ src/data/usecases/index.ts
@@ -1,4 +1,5 @@
 export * from './remote-add-entry';
 export * from './remote-authentication';
-export * from './remote-refresh-token';
 export * from './remote-load-entries-by-month';
+export * from './remote-refresh-token';
+export * from './remote-update-entry';
