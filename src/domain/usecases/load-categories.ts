ac976ccd4f7b93c76bc9a5f912ede3a190773972
Aleksander Sousa <aleksander-ssousa@outlook.com>
2025-10-08 00:47:57 -0300

    feat: introduce LoadCategories interface for loading category data with optional parameters

---
+++ src/domain/usecases/load-categories.ts
@@ -0,0 +1,10 @@
+import { CategoryListResponseModel } from '../models';
+
+export interface LoadCategories {
+  load(params?: LoadCategoriesParams): Promise<CategoryListResponseModel>;
+}
+
+export interface LoadCategoriesParams {
+  type?: 'INCOME' | 'EXPENSE' | 'all';
+  includeStats?: boolean;
+}
