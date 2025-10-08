4c43913f8576187b1f2b288597da53009bc23dcc
Aleksander Sousa <aleksander-ssousa@outlook.com>
2025-10-08 00:48:52 -0300

    feat: implement addCategoryAction for adding categories with user validation and redirection

---
+++ src/presentation/actions/add-category-action.ts
@@ -0,0 +1,33 @@
+'use server';
+
+import { CategoryFormData } from '@/infra/validation';
+import { redirect } from 'next/navigation';
+import { revalidateTag } from 'next/cache';
+import { getCurrentUser } from '../helpers';
+import { makeNextCookiesStorageAdapter } from '@/main/factories/storage/next-cookie-storage-adapter-factory';
+import { makeRemoteAddCategory } from '@/main/factories/usecases/add-category-factory';
+import { logoutAction } from './logout-action';
+
+export async function addCategoryAction(data: CategoryFormData): Promise<void> {
+  try {
+    const getStorage = makeNextCookiesStorageAdapter();
+    const user = await getCurrentUser(getStorage);
+
+    if (!user) {
+      console.warn('User not found, redirecting to logout');
+      await logoutAction();
+      return;
+    }
+
+    const addCategory = makeRemoteAddCategory();
+    await addCategory.add(data);
+
+    revalidateTag('categories');
+    revalidateTag(`categories-${user.id}`);
+
+    redirect('/categories');
+  } catch (error: any) {
+    console.error('Add category error:', error);
+    await logoutAction();
+  }
+}
