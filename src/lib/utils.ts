d2ed2b72bf9f9931936e9205fe40368c0d3b58a7
Aleksander Sousa <aleksander-ssousa@outlook.com>
2025-10-08 02:53:39 -0300

    feat: add utility functions for date formatting and currency conversion

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,24 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const formatDate = (date: Date | string) => {
+  const dateObj = typeof date === 'string' ? new Date(date) : date;
+
+  // Verificar se a data é válida
+  if (isNaN(dateObj.getTime())) {
+    return 'Data inválida';
+  }
+
+  const month = dateObj.getUTCMonth() + 1;
+  const day = dateObj.getUTCDate();
+  const year = dateObj.getUTCFullYear();
+  return `${month}/${day}/${year}`;
+};
+
+export const formatCurrency = (amount: number) => {
+  return new Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+  }).format(amount / 100);
+};
