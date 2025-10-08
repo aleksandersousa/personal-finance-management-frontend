8c767c9b595536db1fb58f07a4857f652fe2589e
Aleksander Sousa <aleksander-ssousa@outlook.com>
2025-10-08 00:47:03 -0300

    feat: implement RemoteAddCategory use case for adding categories via HTTP client

---
+++ src/data/usecases/remote-add-category.ts
@@ -0,0 +1,18 @@
+import { AddCategory, AddCategoryParams } from '@/domain/usecases/add-category';
+import { CategoryModel } from '@/domain/models/category';
+import { HttpClient } from '@/data/protocols/http/http-client';
+
+export class RemoteAddCategory implements AddCategory {
+  constructor(
+    private readonly url: string,
+    private readonly httpClient: HttpClient
+  ) {}
+
+  async add(params: AddCategoryParams): Promise<CategoryModel> {
+    const response = await this.httpClient.post<CategoryModel>(
+      this.url,
+      params
+    );
+    return response;
+  }
+}
