b6e8d3685a6c50c294bb51cc93faa01f87df8077
Aleksander Sousa <aleksander-ssousa@outlook.com>
2025-11-26 20:14:47 -0300

    refactor: remove unused rowsPerPage option from pagination component for cleaner code

--- src/presentation/components/client/pagination.tsx
+++ src/presentation/components/client/pagination.tsx
@@ -94,7 +94,6 @@ export function Pagination({
   const isLastPage = currentPage === totalPages;
 
   const rowsPerPageOptions = [
-    { value: '1', label: '1' },
     { value: '5', label: '5' },
     { value: '10', label: '10' },
     { value: '20', label: '20' },
